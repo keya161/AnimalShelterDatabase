cc435d0925d87ca7a1fdb41a6882fb27530cb102
Kriti Bharadwaj <93143184+gypsybozo@users.noreply.github.com>
2024-10-29 12:29:05 +0530

    model definitions

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,23 +1,117 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, CHAR, Date, Enum
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, CHAR, Date, Enum, Float
 from sqlalchemy.orm import relationship
 from .database import Base
 
+class Employee(Base):
+    __tablename__ = "employee"
+    
+    employee_id = Column(CHAR(5), primary_key=True, nullable=False)
+    name = Column(String(45), nullable=False)
+    date_of_joining = Column(Date, nullable=False)
+    role = Column(String(45), nullable=False)
+    credentials = relationship("Credentials", back_populates="employee", uselist=False)
+
+class Credentials(Base):
+    __tablename__ = "credentials"
+    
+    employee_id = Column(CHAR(5), ForeignKey("employee.employee_id"), primary_key=True, nullable=False)
+    username = Column(String(45), nullable=False)
+    password = Column(String(45), nullable=False)
+    employee = relationship("Employee", back_populates="credentials", uselist=False)
+
 class Animal(Base):
     __tablename__ = "animals"
-
-    animal_ID = Column(CHAR(5), primary_key=True, nullable=False)
-    name = Column(String(40), nullable=False)
-    DOB = Column(Date, nullable=True)
+    
+    animal_id = Column(CHAR(5), primary_key=True, nullable=False)
+    name = Column(String(45), nullable=False)
+    dob = Column(Date, nullable=False)
     gender = Column(Enum("M", "F"), nullable=False)
-    passive_adopter = Column(String(45), nullable=True)
     breed_id = Column(CHAR(5), ForeignKey("type.type_id"), nullable=False)
+    passive_adopter = Column(String(45), nullable=True)
+    
     breed = relationship("Type", back_populates="animals")
+    medical_records = relationship("MedicalRecords", back_populates="animal")
+    supplements = relationship("MedicineInventory", secondary="supplements", back_populates="animals")
+    adopters = relationship("Adopter", back_populates="animal")
 
 class Type(Base):
     __tablename__ = "type"
-
+    
     type_id = Column(CHAR(5), primary_key=True, nullable=False)
-    breed_name = Column(String(45), nullable=False)
-    freq_of_checkup_days = Column(INT, nullable=True)  
-    Bath = Column(Date, nullable=True)
+    breed = Column(String(45), nullable=False)
+    feed = Column(String(45), nullable=False)
+    freq_of_checkup = Column(Integer, nullable=False)
+    bath = Column(Date, nullable=True)
+    
     animals = relationship("Animal", back_populates="breed")
+    food_items = relationship("FoodInventory", secondary="type_for_animals", back_populates="types")
+
+class TypeForAnimals(Base):
+    __tablename__ = "type_for_animals"
+    
+    type_id = Column(CHAR(5), ForeignKey("type.type_id"), primary_key=True)
+    feed_id = Column(CHAR(5), ForeignKey("food_inventory.food_id"), primary_key=True)
+
+class FoodInventory(Base):
+    __tablename__ = "food_inventory"
+    
+    food_id = Column(CHAR(5), primary_key=True, nullable=False)
+    type = Column(String(45), nullable=False)
+    stock = Column(Integer, nullable=False)
+    cost_per_kg = Column(Float, nullable=False)
+    
+    types = relationship("Type", secondary="type_for_animals", back_populates="food_items")
+
+class MedicalRecords(Base):
+    __tablename__ = "medical_records"
+    
+    record_id = Column(CHAR(5), primary_key=True, nullable=False)
+    animal_id = Column(CHAR(5), ForeignKey("animals.animal_id"), nullable=False)
+    name = Column(String(45), nullable=False)
+    report = Column(String(255), nullable=True)
+    doctor = Column(String(45), nullable=False)
+    date = Column(Date, nullable=False)
+    diagnosis = Column(String(255), nullable=False)
+    medicine = Column(String(45), nullable=False)
+    follow_up = Column(Date, nullable=True)
+    freq_of_usage = Column(Integer, nullable=True)
+    
+    animal = relationship("Animal", back_populates="medical_records")
+    medicines = relationship("MedicineInventory", secondary="uses", back_populates="medical_records")
+
+class MedicineInventory(Base):
+    __tablename__ = "medicine_inventory"
+    
+    medicine_id = Column(CHAR(5), primary_key=True, nullable=False)
+    name = Column(String(45), nullable=False)
+    stock = Column(Integer, nullable=False)
+    expiry = Column(Date, nullable=False)
+    date_of_buying = Column(Date, nullable=False)
+    
+    medical_records = relationship("MedicalRecords", secondary="uses", back_populates="medicines")
+    animals = relationship("Animal", secondary="supplements", back_populates="supplements")
+
+class Uses(Base):
+    __tablename__ = "uses"
+    
+    animal_id = Column(CHAR(5), ForeignKey("animals.animal_id"), primary_key=True)
+    medicine_id = Column(CHAR(5), ForeignKey("medicine_inventory.medicine_id"), primary_key=True)
+    record_id = Column(CHAR(5), ForeignKey("medical_records.record_id"), primary_key=True)
+    date = Column(Date, nullable=False)
+
+class Supplements(Base):
+    __tablename__ = "supplements"
+    
+    animal_id = Column(CHAR(5), ForeignKey("animals.animal_id"), primary_key=True)
+    medicine_id = Column(CHAR(5), ForeignKey("medicine_inventory.medicine_id"), primary_key=True)
+
+class Adopter(Base):
+    __tablename__ = "adopter"
+    
+    adopter_id = Column(CHAR(5), primary_key=True, nullable=False)
+    name = Column(String(45), nullable=False)
+    animal_id = Column(CHAR(5), ForeignKey("animals.animal_id"), nullable=False)
+    animal_name = Column(String(45), nullable=False)
+    contribution = Column(Float, nullable=False)
+    
+    animal = relationship("Animal", back_populates="adopters")
