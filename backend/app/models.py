a13704dc30ba68ef0d30113e0bb7666447e137dd
Keya Kesani <kkesani@gmail.com>
2024-10-29 09:31:37 +0530

    setup database-backend connection

---
+++ backend/app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, CHAR, Date, Enum
+from sqlalchemy.orm import relationship
+from database import Base
+
+class Animal(Base):
+    __tablename__ = "animals"
+
+    animal_ID = Column(CHAR(5), primary_key=True, nullable=False)
+    name = Column(String(40), nullable=False)
+    DOB = Column(Date, nullable=True)
+    gender = Column(Enum("M", "F"), nullable=False)
+    passive_adopter = Column(String(45), nullable=True)
